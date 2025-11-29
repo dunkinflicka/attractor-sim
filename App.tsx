f40bfdbea0027d3995e11bf661b4bde4d385b6fb
Anvit <moreanvit2002@gmail.com>
2025-11-29 23:14:15 +0530

    new attractor

--- App.tsx
+++ App.tsx
@@ -51,7 +51,6 @@ const App: React.FC = () => {
       [key]: value
     }));
   };
-
   const handleReset = () => {
      const newParams: Record<string, number> = {};
     Object.keys(activeAttractor.defaultParams).forEach(key => {
